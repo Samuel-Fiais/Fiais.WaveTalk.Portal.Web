1791d8e71980a925a0ac737edfa8c6ab128f2b08
Samuel Fiais <samuelfiais.dev@gmail.com>
2024-05-12 22:02:55 -0300

    [UPDATE] Remove Code unnecessary

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,12 +1,12 @@
-import { AUTH_TOKEN } from "../constants/query-client-constants"
-import { useStorage } from "./useStorage"
+import { AUTH_TOKEN } from "../constants/query-client-constants";
+import { useStorage } from "./useStorage";
 
 export const useLogin = () => {
-  const { getStorageItem, setStorageItem, removeStorageItem } = useStorage()
+  const { getStorageItem } = useStorage();
 
-  const getAuthToken = () => getStorageItem(AUTH_TOKEN)
+  const getAuthToken = () => getStorageItem(AUTH_TOKEN);
 
   return {
-    getAuthToken
-  }
-}
+    getAuthToken,
+  };
+};
